fec4aeffaba2afc95771bff079f01f5078c97189
Bookis Smuin <bookis.smuin@gmail.com>
2015-10-13 10:10:50 -0700

    Add chair with attr_* and defs

--- playground/chair.rb
+++ playground/chair.rb
@@ -1,17 +1,28 @@
 class Chair
+  attr_accessor :name
+  # attr_reader :name, :weight
+  # attr_writer :name, :weight
 
-  def initialize(n, w)
+  def initialize(n,w)
     # define the name of the chair
     @name = n
     @weight = w
   end
 
-  def name
-    @name
+  def yell_name
+    @name.upcase + "!!!!!!"
   end
 
-  def weight
-    @weight
-  end
+  # def name=(new_name)
+  #   @name = new_name
+  # end
+
+  # def weight
+  #   @weight
+  # end
+
+  # def weight=(new_weight)
+  #   @weight = new_weight
+  # end
 
 end
