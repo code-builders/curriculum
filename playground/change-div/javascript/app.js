9da3f319e5ffa24a53ed3d19e776ce937a746fdc
Bookis Smuin <bookis.smuin@gmail.com>
2016-01-13 10:23:10 -0800

    with variable for speed

--- playground/change-div/javascript/app.js
+++ playground/change-div/javascript/app.js
@@ -1,12 +1,13 @@
 $(function() {
+  var speed = 2000;
   $(".box").click(function() {
     if ($(this).width() < 500) {
-      $(".box").animate({width: 500, height: 500}, 2000)
+      $(".box").animate({width: 500, height: 500}, speed)
     } else {
-      $(".box").animate({width: 100, height: 100}, 2000)
+      $(".box").animate({width: 100, height: 100}, speed)
     }
     $(".box").toggleClass("change-color");
   });
-  $(".box").animate({width: 500, height: 500}, 2000)
+  $(".box").animate({width: 500, height: 500}, speed)
   $(".box").addClass("change-color");
 });
