e2409dacc508dfde950118c523f8cd1903fdee94
Bookis Smuin <bookis.smuin@gmail.com>
2016-01-13 10:23:01 -0800

    with size conditional

--- playground/change-div/javascript/app.js
+++ playground/change-div/javascript/app.js
@@ -1,6 +1,10 @@
 $(function() {
   $(".box").click(function() {
-    $(".box").animate({width: 100, height: 100}, 2000)
+    if ($(this).width() < 500) {
+      $(".box").animate({width: 500, height: 500}, 2000)
+    } else {
+      $(".box").animate({width: 100, height: 100}, 2000)
+    }
     $(".box").toggleClass("change-color");
   });
   $(".box").animate({width: 500, height: 500}, 2000)
