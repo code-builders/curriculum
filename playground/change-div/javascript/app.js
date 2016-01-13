2b961faf170e9238b6fbb22caae387378d7cdcff
Bookis Smuin <bookis.smuin@gmail.com>
2016-01-13 10:22:20 -0800

    with toggle

--- playground/change-div/javascript/app.js
+++ playground/change-div/javascript/app.js
@@ -1,7 +1,7 @@
 $(function() {
   $(".box").click(function() {
     $(".box").animate({width: 100, height: 100}, 2000)
-    $(".box").removeClass("change-color");
+    $(".box").toggleClass("change-color");
   });
   $(".box").animate({width: 500, height: 500}, 2000)
   $(".box").addClass("change-color");
