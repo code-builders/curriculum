c765f90a2b4a2f74a50a1b4429011b6b9839a2cf
Bookis Smuin <bookis.smuin@gmail.com>
2016-01-14 10:14:38 -0800

    finished bar chart

--- playground/change-div/javascript/app.js
+++ playground/change-div/javascript/app.js
@@ -1,6 +1,6 @@
 $(function() {
   $(".box-width").change(function() {
-    var width = $(".box-width").val();
-    $(".box").animate({width: width}, 200)
+    var width = $(this).val();
+    $(this).siblings(".box").animate({width: width}, 300)
   });
 });
